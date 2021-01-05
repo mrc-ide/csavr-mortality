3adbb2c044fca2d9cb40f845581585f8eb281f46
osymandius <bolistevens@gmail.com>
2021-01-05 16:21:36 +0000

    Update navigation.R

--- src/ui/navigation.R
+++ src/ui/navigation.R
@@ -1,5 +1,5 @@
 navigationPanel <- function() {
-  navlistPanel(HTML("<b>CSAVR mortality data</b>"),
+  navlistPanel(HTML("<b>CoD visualiser</b>"),
     widths = c(2, 10), well = FALSE,
     tabPanel("Introduction", introduction()),
     tabPanel("Visualise", visualise()),
